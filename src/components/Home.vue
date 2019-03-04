79fef9d73a7055791142d7445272ffeee9f02a11
panwu <panwu@ctrip.com>
2019-03-04 14:35:54 +0800

    add recommed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,8 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
@@ -10,12 +12,17 @@
 import HomeHeader from './home/header.vue'
 import HomeSwiper from './home/Swiper'
 import HomeIcons from './home/icons'
+import HomeRecommend from './home/recommend'
+import HomeWeekend from './home/weekend'
+
 export default {
   name: "Home",
   components:{
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 };
 </script>
---
+++ src/components/home/weekend.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of recommedList"
+        :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img
+            class="item-img"
+            :src="item.imgUrl"
+          />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "HomeWeekend",
+  data() {
+    return {
+      recommedList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg",
+          title: "深圳野生动物园",
+          desc: "山清水秀的深圳西丽湖畔"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg",
+          title: "深圳野生动物园",
+          desc: "山清水秀的深圳西丽湖畔"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg",
+          title: "深圳野生动物园",
+          desc: "山清水秀的深圳西丽湖畔"
+        },
+        {
+          id: "0004",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg",
+          title: "深圳野生动物园",
+          desc: "山清水秀的深圳西丽湖畔"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+
+.title
+  margin-top 0.1rem
+  line-height 0.8rem
+  background #eeeeee
+  text-indent 0.2rem
+.item-img-wrapper
+  overflow hidden
+  height 0
+  padding-bottom 33.9%
+  .item-img
+    width 100%
+.item-info
+  padding 0.1rem
+  .item-title
+    line-height 0.54rem
+    font-size 0.32rem
+    ellipsis()
+  .item-desc
+    line-height 0.4rem
+    color #cccccc
+    ellipsis()
+</style>
